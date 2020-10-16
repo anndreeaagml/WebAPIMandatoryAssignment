a284adc319546f2e50797588cf1588e2d828b2f9
Andreea Vasiliu <andr77c6@edu.easj.dk>
2020-10-16 10:12:11 +0200

    Add project files.

---
+++ WebAPIAssignment/Controllers/BooksController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using BookLibraryAssignemnt;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebAPIAssignment.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPIAssignment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+
+
+        private static List<Book> lBooks;
+
+        public BooksController()
+        {
+            lBooks = BookModel.BookList;
+        }
+
+
+        // GET: api/<BookController>
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return lBooks;
+        }
+
+        // GET api/<BookController>/5
+        [HttpGet("{isbn13}")]
+        public IActionResult Get(string isbn13)
+        {
+            var book = BookModel.GetBook(isbn13);
+            if (book != null)
+            {
+                return Ok(book);
+            }
+            else
+            {
+                return NotFound(new { message = "Isbn not Found!" });
+            }
+        }
+
+        // POST api/<BookController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Book newBook)
+        {
+
+            if (!BookExists(newBook.Isbn))
+            {
+                lBooks.Add(newBook);
+                return CreatedAtAction("Get", new { Isbn13 = newBook.Isbn }, newBook);
+            }
+            else
+            {
+                return NotFound(new { message = "Isbn13 is duplicate" });
+            }
+
+        }
+
+        // PUT api/<BookController>/5
+        [HttpPut("{Isbn13}")]
+        public IActionResult Put(string Isbn13, [FromBody] Book updatedBook)
+        {
+            if (Isbn13 != updatedBook.Isbn)
+            {
+                return BadRequest();
+            }
+
+            Book currentBook = BookModel.GetBook(Isbn13);
+
+            if (currentBook != null)
+            {
+                lBooks.Remove(currentBook);
+                lBooks.Add(updatedBook);
+
+            }
+            else
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        // DELETE api/<BookController>/5
+        [HttpDelete("{isbn13}")]
+        public IActionResult Delete(string isbn13)
+        {
+            var book = BookModel.GetBook(isbn13);
+            if (book != null)
+            {
+                lBooks.Remove(book);
+            }
+            else
+            {
+                return NotFound();
+            }
+
+            return Ok(book);
+        }
+
+
+        // Helper method
+
+        private bool BookExists(string isbn13)
+        {
+            return lBooks.Any(b => b.Isbn == isbn13);
+        }
+    }
+}
---
+++ WebAPIAssignment/Model/BookModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using BookLibraryAssignemnt;
+using BookLibraryAssignemnt;
+
+namespace WebAPIAssignment.Model
+{
+    public class BookModel
+    {
+        public static List<Book> BookList = new List<Book>()
+        {
+            new Book("They were none", "Agatha Christie", 203, "c84y2424ncmr2"),
+            new Book("Murder at the vicariage", "Agatha Christie", 223, "c9825cmv25mp2"),
+            new Book("Mathias Sandorf", "Jules Vernes", 950, "mc89p42wc48v5"),
+
+        };
+
+        public static Book GetBook(string isbn13)
+        {
+            Book book = BookList.FirstOrDefault(b => b.Isbn == isbn13);
+            return book;
+        }
+
+        
+    }
+   
+}
